3930694d8fe3446f3feb1c48e0b6b741d3b87ee9
RJW <blacksheeprobotics@gmail.com>
2024-01-31 00:18:31 -0500

    Added TODO list

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+#TODO 1. Print report
+#TODO 2. Check sufficient resources available
+#TODO 3. Process coins
+#TODO 4. Check if transaction was successful
+#TODO 5. Make coffee
+
