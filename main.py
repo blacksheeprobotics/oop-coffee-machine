3e6ceafe4c45e25b247f52fce85ac25c0e84f10a
RJW <blacksheeprobotics@gmail.com>
2024-01-31 22:42:26 -0500

    Added menu selection to user input: report/off/menu item.

--- main.py
+++ main.py
@@ -2,9 +2,30 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-#TODO 1. Print report
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+#TODO 1. Print report -> DONE
 #TODO 2. Check sufficient resources available
+
 #TODO 3. Process coins
 #TODO 4. Check if transaction was successful
 #TODO 5. Make coffee
 
+continue_to_order = True
+while continue_to_order:
+    drink_order = input("Which drink would you like? (espresso/latte/cappuccino): ").lower()
+    if drink_order == "report":
+        coffee_maker.report()
+    elif drink_order == "off":
+        continue_to_order = False
+    elif drink_order == "espresso" or drink_order == "latte" or drink_order == "cappuccino":
+        drink = menu.find_drink(drink_order)
+        if coffee_maker.is_resource_sufficient(drink):
+            print("RESOURCES AVAILABLE")
+            
+        else:
+            print("RESOURCES NOT AVAILABLE")
+    else:
+        print("Input is not a valid drink order")
